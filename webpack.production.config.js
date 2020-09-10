6b5099f99dbf651236b3923475b36c55ead48c75
LOGIC <376693576@qq.com>
2020-09-10 16:15:44 +0800

    update 1.1.4 包版本更新，webpack => globalObject: 'this'

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -14,6 +14,7 @@ module.exports = {
     publicPath: '/dist/',
     library: 'vue-puzzle-vcode',
     libraryTarget: 'umd',
+    globalObject: 'this'
     // umdNamedDefine: true,
   },
   externals: {
